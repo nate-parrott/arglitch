f2faed132aee11493239f1993c7c4f6b1f0e3bf8
= <nateparro2t@gmail.com>
2017-10-04 01:14:16 -0400

    Initial Commit

---
+++ ARGlitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARGlitch
+//
+//  Created by Nate Parrott on 10/4/17.
+//  Copyright © 2017 Nate Parrott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
